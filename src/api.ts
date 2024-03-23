53e55302609390ea17223789dbbc0f7930e73e7e
Cside <cside.story@gmail.com>
2024-03-23 20:46:57 +0900

    fix

--- src/api.ts
+++ src/api.ts
@@ -1,5 +1,9 @@
 import ky from "ky";
 
-export const getCount = async () => {
-  return await ky.get("https://api.todoist.com/rest/v2/tasks?project_id=660066260").json();
+export const getTasksCount = async () => {
+  const tasks: unknown[] = await ky
+    .get("https://api.todoist.com/rest/v2/tasks?project_id=660066260")
+    .json();
+  console.log(tasks);
+  return tasks.length;
 };
