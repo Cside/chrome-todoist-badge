9157e249b0a862b67cc20437482cf31186f0fc2f
Cside <cside.story@gmail.com>
2024-03-24 15:41:00 +0900

    max retry

--- src/api.ts
+++ src/api.ts
@@ -1,8 +1,15 @@
 import ky from "ky";
+import { API_BASE_URL } from "./constants/urls";
 
-export const getTasksCount = async () => {
+const MAX_RETRY = 3;
+
+export const getTasksCountWithRetry = async () => {
   const tasks: unknown[] = await ky
-    .get("https://api.todoist.com/rest/v2/tasks?project_id=660066260")
+    .get(`${API_BASE_URL}/rest/v2/tasks?project_id=660066260`, {
+      retry: {
+        limit: MAX_RETRY,
+      },
+    })
     .json();
   console.log(tasks);
   return tasks.length;
