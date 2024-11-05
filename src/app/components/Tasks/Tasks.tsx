4fbda920bf73034dc2e60942f3bc91b938bf2403
Cside <cside.story@gmail.com>
2024-11-05 15:50:20 +0900

    add donation button

--- src/app/components/Tasks/Tasks.tsx
+++ src/app/components/Tasks/Tasks.tsx
@@ -126,7 +126,7 @@ export default function Tasks_Suspended() {
             height={ICON_LENGTH}
             className="m-0"
           />
-          Open Todoist on Web
+          Open Todoist
         </a>
         <NavLink to="/options" className="btn btn-primary px-3">
           <svg
@@ -134,13 +134,29 @@ export default function Tasks_Suspended() {
             width={ICON_LENGTH}
             height={ICON_LENGTH}
             fill="currentColor"
-            className="bi bi-gear-fill"
             viewBox="0 0 16 16"
           >
             <path d="M9.405 1.05c-.413-1.4-2.397-1.4-2.81 0l-.1.34a1.464 1.464 0 0 1-2.105.872l-.31-.17c-1.283-.698-2.686.705-1.987 1.987l.169.311c.446.82.023 1.841-.872 2.105l-.34.1c-1.4.413-1.4 2.397 0 2.81l.34.1a1.464 1.464 0 0 1 .872 2.105l-.17.31c-.698 1.283.705 2.686 1.987 1.987l.311-.169a1.464 1.464 0 0 1 2.105.872l.1.34c.413 1.4 2.397 1.4 2.81 0l.1-.34a1.464 1.464 0 0 1 2.105-.872l.31.17c1.283.698 2.686-.705 1.987-1.987l-.169-.311a1.464 1.464 0 0 1 .872-2.105l.34-.1c1.4-.413 1.4-2.397 0-2.81l-.34-.1a1.464 1.464 0 0 1-.872-2.105l.17-.31c.698-1.283-.705-2.686-1.987-1.987l-.311.169a1.464 1.464 0 0 1-2.105-.872zM8 10.93a2.929 2.929 0 1 1 0-5.86 2.929 2.929 0 0 1 0 5.858z" />
           </svg>
           Change task filters
         </NavLink>
+        <a
+          href="https://ko-fi.com/Cside"
+          target="_blank"
+          className="btn flex columns-3 gap-x-1 px-3"
+          rel="noreferrer"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="#fc3e30"
+            viewBox="0 0 16 16"
+          >
+            <path d="M4 1c2.21 0 4 1.755 4 3.92C8 2.755 9.79 1 12 1s4 1.755 4 3.92c0 3.263-3.234 4.414-7.608 9.608a.513.513 0 0 1-.784 0C3.234 9.334 0 8.183 0 4.92 0 2.755 1.79 1 4 1" />
+          </svg>
+          Donation
+        </a>
       </div>
       {(areTasksFetching || areSectionsFetching) && (
         <Spinner className="fixed top-7 right-7" />
