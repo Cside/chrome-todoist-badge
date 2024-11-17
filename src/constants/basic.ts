a6c1c453efdca533e402441100a6afb077a06306
Cside <cside.story@gmail.com>
2024-11-17 11:05:18 +0900

    DRY

--- src/constants/basic.ts
+++ src/constants/basic.ts
@@ -1,2 +1,8 @@
-// messages.json と重複しているが、/public/* のファイルは import できないため。まぁもう変わらないだろうし⋯
-export const EXTENSION_NAME = "Counter for Todoist";
+import messages from "@/public/_locales/en/messages.json";
+
+export const EXTENSION_NAME = (() => {
+  const name = messages.extensionName.message;
+  if (/[-:：]/.test(name))
+    throw new Error(`Invalid character in extension name: "${name}"`);
+  return name;
+})();
