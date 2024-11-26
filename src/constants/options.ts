a1ec62c3c36b400dc480653d08acc0aad206fe26
Cside <cside.story@gmail.com>
2024-11-26 10:36:36 +0900

    comment

--- src/constants/options.ts
+++ src/constants/options.ts
@@ -4,6 +4,8 @@
 export const DEFAULT_FILTER_BY_DUE_BY_TODAY = false;
 export const DEFAULT_IS_CONFIG_INITIALIZED = false;
 
+// storage への保存のために便宜的に使う ID 。
+// Storage に「 sectionId: !/* 」とかあっても、何のこっちゃ分からんので。。
 export const SECTION_ID_FOR_STORAGE = {
   // biome-ignore format:
   ALL:       "__all", // storage には保存しない。Options.tsx でのみ使う。undefined は disabled を選択状態にすることができないため
