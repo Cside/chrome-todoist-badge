9298fded6841f9703c38b7b931919a83eccf5355
Cside <cside.story@gmail.com>
2024-12-03 14:55:43 +0900

    4XX は TQ で retry させない

--- src/app/components/Providers/QueryClientProvider.tsx
+++ src/app/components/Providers/QueryClientProvider.tsx
@@ -2,18 +2,39 @@ import {
   QueryClient,
   QueryClientProvider as _QueryClientProvider,
 } from "@tanstack/react-query";
+import { HTTPError } from "ky";
 import type { ReactNode } from "react";
-import { MAX_RETRY } from "../../../constants/maxRetry";
+import { MAX_RETRIES } from "../../../constants/maxRetry";
+
+// ky に揃える
+// https://github.com/sindresorhus/ky/blob/3ba40cc6333cf1847c02c51744e22ab7c04407f5/source/utils/normalize.ts#L10
+const RETRY_STATUS_CODES = [408, 413, 429, 500, 502, 503, 504];
 
 const queryClient = new QueryClient({
   // https://tanstack.com/query/latest/docs/reference/QueryClient
   defaultOptions: {
     mutations: {
       throwOnError: true,
-      retry: MAX_RETRY,
+      retry: MAX_RETRIES,
     },
     queries: {
-      retry: MAX_RETRY,
+      retry: (_failureCount, error) => {
+        const failureCount = _failureCount + 1; // failureCount: 0 で始まるため
+
+        if (failureCount > MAX_RETRIES) return false;
+
+        if (
+          error instanceof HTTPError &&
+          !RETRY_STATUS_CODES.includes(error.response.status)
+        ) {
+          console.info(
+            `Aborting retry due to status code: ${error.response.status}`,
+          );
+          return false;
+        }
+        console.info(`Retrying... failureCount: ${failureCount}`);
+        return true;
+      },
       staleTime: Number.POSITIVE_INFINITY,
       gcTime: Number.POSITIVE_INFINITY,
       throwOnError: true,
