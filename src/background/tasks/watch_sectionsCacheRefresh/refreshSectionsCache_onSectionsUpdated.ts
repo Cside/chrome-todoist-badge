37a82cc8e2b963bc281deb057c005d20f6d4263b
Cside <cside.story@gmail.com>
2025-01-31 16:13:17 +0900

    project_delete のときキャッシュを更新

--- src/background/tasks/watch_sectionsCacheRefresh/refreshSectionsCache_onSectionsUpdated.ts
+++ src/background/tasks/watch_sectionsCacheRefresh/refreshSectionsCache_onSectionsUpdated.ts
@@ -3,11 +3,15 @@ import { addCommandListener } from "../fn/addCommandListener";
 import * as api from "./fn/refreshSectionsCache";
 
 // https://www.notion.so/97e7a56e72894496add5f649c56c78bd
-const REGEXP = /^section_/;
+const REGEXP = [
+  // https://www.notion.so/97e7a56e72894496add5f649c56c78bd
+  "^section_",
+  "^project_delete$",
+];
 
 export const refreshSectionsCache_onSectionsUpdated = () =>
   addCommandListener({
     name: "refreshSectionsCache",
-    commandRegExp: REGEXP,
+    commandRegExp: new RegExp(REGEXP.join("|")),
     listener: async () => await api.refreshSectionsCache(),
   });
