8233fb0a6b552fc98bbd53e64c02cfef3a3ee710
Cside <cside.story@gmail.com>
2026-02-21 18:03:40 +0900

    sections

--- src/api/sections/getSections.ts
+++ src/api/sections/getSections.ts
@@ -2,5 +2,10 @@ import type { Api, ProjectId } from "../../types";
 import { ky } from "../ky";
 import { getApiUrlForGetSections } from "./getUrlForGetSections";
 
+// TODO cursor... まぁ50件以上のsectionを持っている人は少ないと思うので、ひとまずは実装しない
 export const getSections = async ({ projectId }: { projectId: ProjectId }) =>
-  await ky.fetchAndNormalize<Api.Section[]>(getApiUrlForGetSections({ projectId }));
+  (
+    await ky.fetchAndNormalize<{ results: Api.Section[] }>(
+      getApiUrlForGetSections({ projectId }),
+    )
+  ).results;
